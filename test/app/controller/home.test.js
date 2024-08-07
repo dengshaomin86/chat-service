df9a8d562879e3d3505c1609e996c3b4a3916947
dengshaomin86 <1101869752@qq.com>
2024-08-07 09:23:47 +0800

    version upgrade

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,17 +1,20 @@
-'use strict';
-
-const { app, assert } = require('egg-mock/bootstrap');
+const { strict: assert } = require('node:assert');
+const path = require('node:path');
+const { statSync } = require('node:fs');
+const { app } = require('egg-mock/bootstrap');
 
 describe('test/app/controller/home.test.js', () => {
-  it('should assert', () => {
+  it('should assert', async () => {
     const pkg = require('../../../package.json');
     assert(app.config.keys.startsWith(pkg.name));
-
-    // const ctx = app.mockContext({});
-    // yield ctx.service.xx();
   });
 
-  it('should GET /', () => {
+  it('should typings exists', async () => {
+    const typings = path.join(__dirname, '../../../typings');
+    assert(statSync(typings));
+  });
+
+  it('should GET /', async () => {
     return app.httpRequest()
       .get('/')
       .expect('hi, egg')
